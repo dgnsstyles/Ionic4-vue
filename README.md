52e546813a2090a9c275fe8e83a65320bfd15098
usuario <usuario@MacBook-Pro-de-usuario-2.local>
2017-10-16 19:31:01 -0300

    final boilerplate for the project

---
+++ README.md
@@ -0,0 +1,18 @@
+# i-vue
+
+> Un projecto de Vue y Ionic
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+   <ion-app>
+    <ion-page class="show-page">
+      <ion-header>
+       <ion-toolbar>
+         <ion-title>Ionic4 + Vue</ion-title>
+       </ion-toolbar>
+      </ion-header>
+      <ion-content>
+       <ion-list>
+        <ion-item v-for="user of users" v-bind:key="user.id">
+          <ion-label>{{user.name}}</ion-label>
+        </ion-item>
+       </ion-list>
+      </ion-content>
+    </ion-page>
+   </ion-app>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'app',
+  data () {
+    return {
+      users: []
+    }
+  },
+  created() {
+      axios.get('http://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        this.users = response.data
+      })
+  }
+}
+</script>
+
+<style>
+
+</style>
